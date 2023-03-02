62f0a991b3a59bbc14c7d4fa674a749e57dde056
TatevEloyan <tatev_eloyan@mail.ru>
2023-03-02 12:30:24 +0400

    file preparation

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import sys
-
 from file_reader import read_file_info_list, read_file_into_dictionary
 from helper import replace_values_and_return_list, count_changes_and_print_sorted_list
 
 if __name__ == '__main__':
-    # args = sys.argv[1:]
-    # text_file = args[0]
-    # config = args[1]
     list = read_file_info_list('text')
     list1 = read_file_info_list('text')
     dic = read_file_into_dictionary('config')
